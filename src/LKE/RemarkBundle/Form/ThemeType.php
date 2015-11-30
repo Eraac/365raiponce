9c424b8d42a8fc25eebf42f36f1a1c9bd5aa4103
Eraac <kevin@labesse.me>
2015-12-01 00:36:31 +0100

    form theme & emotion

---
+++ src/LKE/RemarkBundle/Form/ThemeType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LKE\RemarkBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ThemeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LKE\RemarkBundle\Entity\Theme'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'lke_remarkbundle_theme';
+    }
+}
