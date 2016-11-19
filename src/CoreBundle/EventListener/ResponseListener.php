9aa13c8249d5f0331eb458c8c413b1c35feaf91d
Eraac <kevin@labesse.me>
2016-11-19 16:25:00 +0100

    auto publish response

---
+++ src/CoreBundle/EventListener/ResponseListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CoreBundle\EventListener;
+
+use CoreBundle\Entity\Response;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+class ResponseListener
+{
+    public function prePersistHandler(Response $response, LifecycleEventArgs $event)
+    {
+        if ($this->isValidUser($response)) {
+            $response->setPostedAt(new \DateTime());
+        }
+    }
+
+    /**
+     * @param Response $response
+     *
+     * @return bool
+     */
+    private function isValidUser(Response $response) : bool
+    {
+        $user = $response->getAuthor();
+
+        return is_null($user) ? false : $user->isConfirmed();
+    }
+}
