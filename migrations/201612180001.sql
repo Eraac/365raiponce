9b9556f5168724733680cf2f71ea35c16347682c
Eraac <kevin@labesse.me>
2017-02-03 21:10:09 +0100

    update script migration with new tables api v2

--- migrations/201612180001.sql
+++ migrations/201612180001.sql
@@ -1,7 +1,10 @@
 -- clean when api v1 will not more exist
 ALTER TABLE `user`
-  DROP expired,
-  DROP credentials_expired;
+    DROP locked,
+    DROP expired,
+    DROP expires_at,
+    DROP credentials_expired,
+    DROP credentials_expire_at;
 
 ALTER TABLE `theme` DROP slug;
 ALTER TABLE `emotion` DROP slug;
