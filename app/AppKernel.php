26a15909828c57977b026c9a0a6ac0ac81e38cb1
Eraac <kevin@labesse.me>
2017-02-06 22:52:56 +0100

    invalid cache count vote

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,11 +26,8 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new Snc\RedisBundle\SncRedisBundle(),
         ];
-
-        if ('prod' === $this->getEnvironment()) {
-            $bundles[] = new Snc\RedisBundle\SncRedisBundle();
-        }
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
--- src/CoreBundle/EventListener/Doctrine/VoteListener.php
+++ src/CoreBundle/EventListener/Doctrine/VoteListener.php
@@ -3,8 +3,11 @@
 namespace CoreBundle\EventListener\Doctrine;
 
 use CoreBundle\Entity\AbstractVote;
+use CoreBundle\Entity\VoteRemark;
 use CoreBundle\Event\NewVoteEvent;
+use CoreBundle\Service\KeyBuilder;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Predis\Client;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class VoteListener
@@ -14,10 +17,22 @@ class VoteListener
      */
     private $dispatcher;
 
+    /**
+     * @var Client
+     */
+    private $redis;
 
-    public function __construct(EventDispatcherInterface $dispatcher)
+
+    /**
+     * VoteListener constructor.
+     *
+     * @param EventDispatcherInterface $dispatcher
+     * @param Client                   $redis
+     */
+    public function __construct(EventDispatcherInterface $dispatcher, Client $redis)
     {
         $this->dispatcher = $dispatcher;
+        $this->redis = $redis;
     }
 
     /**
@@ -29,5 +44,28 @@ class VoteListener
         $event = new NewVoteEvent($vote);
 
         $this->dispatcher->dispatch(NewVoteEvent::NAME, $event);
+
+        $this->invalideCache($vote);
+    }
+
+    /**
+     * @param AbstractVote $vote
+     * @param LifecycleEventArgs $event
+     */
+    public function preRemoveHandler(AbstractVote $vote, LifecycleEventArgs $event)
+    {
+        $this->invalideCache($vote);
+    }
+
+    /**
+     * @param AbstractVote $vote
+     */
+    private function invalideCache(AbstractVote $vote)
+    {
+        $key = $vote instanceof VoteRemark ?
+            KeyBuilder::keyCountVoteForRemark($vote->getRemark(), $vote->getType()) :
+            KeyBuilder::keyCountVoteForResponse($vote->getResponse());
+
+        $this->redis->del($key);
     }
 }
