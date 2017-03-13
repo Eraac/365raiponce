38e7cdce325c735d21a8569704d7430836586696
Eraac <kevin@labesse.me>
2017-03-13 13:41:58 +0100

    cast string to int /stats+/*

--- src/CoreBundle/CoreBundle.php
+++ src/CoreBundle/CoreBundle.php
@@ -2,8 +2,18 @@
 
 namespace CoreBundle;
 
+use Doctrine\DBAL\Types\{IntegerType, Type};
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CoreBundle extends Bundle
 {
+    /**
+     * @inheritdoc
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        Type::overrideType('boolean', IntegerType::class);
+    }
 }
