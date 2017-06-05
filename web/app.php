92c7fd0ab4b0f2a07334a0792fd7e9df2b1b32f7
Eraac <kevin@labesse.me>
2017-06-05 07:36:54 +0200

    add trusted proxies (traefik)

--- web/app.php
+++ web/app.php
@@ -8,7 +8,11 @@ include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
+
 $request = Request::createFromGlobals();
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedProxies(array('127.0.0.1', $request->server->get('REMOTE_ADDR')));
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
